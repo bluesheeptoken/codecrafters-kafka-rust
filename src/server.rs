4ac8e3617b9b766d4c3cf30008202f54aa0ad9af
Louis Fruleux <louis.fruleux1@gmail.com>
2024-09-23 17:51:14 +0200

    pass second test

--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,27 @@
-use std::io::{self, Write};
+use std::io::{self, Read, Write};
 use std::net::TcpListener;
 
-fn write_api_versions<T: Write>(mut stream: T) -> io::Result<usize> {
-    let buf: [u8; 8] = [0, 0, 0, 0, 0, 0, 0, 7];
-    stream.write(&buf)
+use bytes::{Buf, BufMut};
+
+fn handle_connection<T: Read + Write>(mut stream: T) -> io::Result<usize> {
+    let mut len = [0; 4];
+    stream.read_exact(&mut len)?;
+    let len = i32::from_be_bytes(len) as usize;
+
+    let mut request = vec![0; len];
+    stream.read_exact(&mut request)?;
+
+    let mut request = request.as_slice();
+
+    let _request_api_key = request.get_i16();
+    let _request_api_version = request.get_i16();
+    let correlation_id: i32 = request.get_i32();
+
+    let mut response: Vec<u8> = Vec::with_capacity(8);
+    response.put_i32(0);
+    response.put_i32(correlation_id);
+
+    stream.write(&response)
 }
 
 pub fn start_server(address: &str) -> io::Result<()> {
@@ -14,7 +32,7 @@ pub fn start_server(address: &str) -> io::Result<()> {
         match stream {
             Ok(s) => {
                 println!("Accepted new connection");
-                let _size = write_api_versions(s)?;
+                handle_connection(s)?;
             }
             Err(e) => {
                 println!("Error: {}", e);
@@ -31,11 +49,23 @@ mod tests {
 
     #[test]
     fn test_handle_connection() {
-        let mut cursor: Cursor<Vec<u8>> = Cursor::new(vec![]);
+        let mut request: Vec<u8> = vec![];
+        request.put_i32(35);
+        request.put_i16(18);
+        request.put_i16(4);
+        request.put_i32(311908132);
+        request.put_bytes(0, 27);
+        dbg!(&request);
+        let mut cursor: Cursor<Vec<u8>> = Cursor::new(request);
 
-        let result = write_api_versions(&mut cursor);
+        let result = handle_connection(&mut cursor);
 
         assert!(result.is_ok());
-        assert_eq!(cursor.get_ref().as_slice(), &[0, 0, 0, 0, 0, 0, 0, 7]);
+        assert_eq!(result.unwrap(), 8);
+
+        assert_eq!(
+            &cursor.get_ref()[39..47],
+            &vec![0, 0, 0, 0, 18, 151, 87, 36][..]
+        );
     }
 }
